cab55753df8894d14d6e4c121acfcf6145c4a2cb
G1llar <przmemyslaw.kwiecinski91@gmail.com>
2026-03-27 09:47:05 +0100

    DEV-8 #comment Przygotowanie połączenia z PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.db.session import engine
 
 app = FastAPI(title="FindMyMate API")
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.get("/db-check")
+def db_check():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        values = result.scalar()
+
+    return {"database": "connected", "status": values}
