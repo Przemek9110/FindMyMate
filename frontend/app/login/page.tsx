fbfba671c53e36fa762ac79f82e7dbdbc8c0c626
karoq18 <karolinacwiklinska93@wp.pl>
2026-03-29 10:16:08 +0200

    DEV-24 konfiguracja i test Zustanda #done

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,3 +1,34 @@
-export default function loginPage() {
-  return <div>login page</div>;
+"use client";
+
+import { useAuthStore } from "@/store/authStore";
+
+export default function LoginPage() {
+  const setAuth = useAuthStore((s) => s.setAuth);
+  const user = useAuthStore((s) => s.user);
+  const logout = useAuthStore((s) => s.logout);
+
+  return (
+    <div>
+      login page
+
+      <button
+        onClick={() =>
+          setAuth({
+            user: {
+              id: "1",
+              username: "Karo",
+              email: "test@test.pl",
+            },
+            token: "123",
+          })
+        }
+      >
+        TEST LOGIN
+      </button>
+
+      <p>{user?.username}</p>
+
+      <button onClick={logout}>LOGOUT</button>
+    </div>
+  );
 }
